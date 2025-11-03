92e5e3505e39dfec6d293f56021c1fdcf132c430
Andrew-Ryer <aryer@csumb.edu>
2025-11-02 23:15:37 -0800

    Tests

--- app/src/test/java/com/example/hw04_gymlog_v300/UnitTest.java
+++ app/src/test/java/com/example/hw04_gymlog_v300/UnitTest.java
@@ -4,12 +4,16 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+import android.app.Application;
+
+import com.example.hw04_gymlog_v300.database.GymLogRepository;
+
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
-public class ExampleUnitTest {
+public class UnitTest {
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
